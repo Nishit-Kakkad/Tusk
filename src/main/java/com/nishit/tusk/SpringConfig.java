1cb977015435109b1eca801bd1ec2c916d42d302
Nishit-Kakkad <kakkadnishit95@gmail.com>
2018-06-27 17:45:23 +0530

    Added Hibernate

--- src/main/java/com/nishit/tusk/SpringConfig.java
+++ src/main/java/com/nishit/tusk/SpringConfig.java
@@ -5,9 +5,19 @@
  */
 package com.nishit.tusk;
 
+import com.mchange.v2.c3p0.DriverManagerDataSource;
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -18,8 +28,14 @@ import org.springframework.web.servlet.view.JstlView;
  * @author Nishit-TCS
  */
 @Configuration
+@EnableTransactionManagement
 @ComponentScan(basePackages = {"com.nishit.tusk.Controller", "com.nishit.tusk.dao", "com.nishit.tusk.entity"})
+@PropertySource(value = { "classpath:application.properties" })
 public class SpringConfig implements WebMvcConfigurer{
+    
+    @Autowired
+    private Environment environment;
+    
     @Bean
     public ViewResolver resolver() {
         InternalResourceViewResolver resolve = new InternalResourceViewResolver();
@@ -27,5 +43,41 @@ public class SpringConfig implements WebMvcConfigurer{
         resolve.setPrefix("");
         resolve.setSuffix(".jsp");
         return resolve;
+    }
+    
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource());
+        localSessionFactoryBean.setPackagesToScan(new String[] { "com.nishit.tusk.entity" });
+        localSessionFactoryBean.setHibernateProperties(hibernateProperties());
+        return localSessionFactoryBean;
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/college");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    } 
+    
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.connection.pool_size", 20);
+        return properties;
+    }
+    
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory s) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager(s);
+        return txManager;
     }
 }
